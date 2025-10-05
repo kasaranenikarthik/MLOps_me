054c81f09df662d3cbb2ef913e4b2371ddcbb7dd
karthikkgvs <kasaraneni.g@northeastern.edu>
2025-10-05 17:21:44 -0400

    Added FastAPI Lab

--- Labs/API_Labs/FastAPI_Labs/src/main.py
+++ Labs/API_Labs/FastAPI_Labs/src/main.py
@@ -5,27 +5,36 @@ from predict import predict_data
 
 app = FastAPI()
 
-class IrisData(BaseModel):
-    petal_length: float
-    sepal_length: float
-    petal_width: float
-    sepal_width: float
+class DiabetesData(BaseModel):
+    age: float
+    sex: float
+    bmi: float
+    bp: float
+    s1: float
+    s2: float
+    s3: float
+    s4: float
+    s5: float
+    s6: float
 
-class IrisResponse(BaseModel):
-    response:int
+class DiabetesResponse(BaseModel):
+    response: int
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def health_ping():
     return {"status": "healthy"}
 
-@app.post("/predict", response_model=IrisResponse)
-async def predict_iris(iris_features: IrisData):
+@app.post("/predict", response_model=DiabetesResponse, status_code=status.HTTP_200_OK)
+async def predict_diabetes(diabetes_features: DiabetesData):
     try:
-        features = [[iris_features.sepal_length, iris_features.sepal_width,
-                    iris_features.petal_length, iris_features.petal_width]]
+        features = [[diabetes_features.age, diabetes_features.sex,
+                    diabetes_features.bmi, diabetes_features.bp,
+                    diabetes_features.s1, diabetes_features.s2,
+                    diabetes_features.s3, diabetes_features.s4,
+                    diabetes_features.s5, diabetes_features.s6]]
 
         prediction = predict_data(features)
-        return IrisResponse(response=int(prediction[0]))
+        return DiabetesResponse(response=int(prediction[0]))
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
